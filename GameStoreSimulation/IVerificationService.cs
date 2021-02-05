5e03475e9d6e8883912bdc852ac09b67d1f2e787
iremaltnz <55363753+iremaltnz@users.noreply.github.com>
2021-02-06 02:18:16 +0300

    Game Store Simulation

---
+++ GameStoreSimulation/IVerificationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    interface IVerificationService
+    {
+       bool Dogrulama(Gamer gamer);
+    }
+}
